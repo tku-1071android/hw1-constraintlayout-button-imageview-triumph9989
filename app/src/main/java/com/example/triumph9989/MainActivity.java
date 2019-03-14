964d386b05a496984aedc8dee6e89699f6575314
triumph <justin579721@kimo.com>
2019-03-14 13:11:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/triumph9989/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.triumph9989;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    int size = 30;//sp
+
+    public void bigger(view v){
+
+    }
+}
